bc576f9ff4ed051ae2f3e0934ffb0c46110cd7ac
Vic <thangson62@gmail.com>
2025-03-25 16:21:59 +0700

    Initial commit

---
+++ src/main/java/com/example/learning_management_system_api/LearningManagementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.learning_management_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningManagementSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningManagementSystemApiApplication.class, args);
+	}
+
+}

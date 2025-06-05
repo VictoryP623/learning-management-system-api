661d968ccebfed7160ad6645d5e533627f8bf2bb
Vic <thangson62@gmail.com>
2025-06-06 03:46:28 +0700

    chỉnh lại user request

--- src/main/java/com/example/learning_management_system_api/dto/request/UserRequestDto.java
+++ src/main/java/com/example/learning_management_system_api/dto/request/UserRequestDto.java
@@ -1,6 +1,21 @@
 package com.example.learning_management_system_api.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
 
-public record UserRequestDto(String fullname, @Past LocalDateTime birthdate, String address) {}
+public record UserRequestDto(
+    @NotBlank(message = "Họ tên không được để trống")
+        @Size(min = 3, max = 50, message = "Họ tên phải từ 3-50 ký tự")
+        @Pattern(
+            regexp = "^[a-zA-ZÀ-ỹ\\s]+$",
+            message = "Họ tên chỉ được chứa chữ cái và khoảng trắng")
+        String fullname,
+    @Past(message = "Ngày sinh phải là một ngày trong quá khứ") LocalDateTime birthdate,
+    @Size(max = 100, message = "Địa chỉ tối đa 100 ký tự")
+        @Pattern(
+            regexp = "^[a-zA-Z0-9À-ỹ\\s,.-/]+$",
+            message = "Địa chỉ không được chứa ký tự đặc biệt")
+        String address) {}
